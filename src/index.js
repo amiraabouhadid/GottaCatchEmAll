dba5319d35a14a3045a4a306d5eedd530c0476c8
amira <amiraabouhadid@gmail.com>
2022-03-07 13:52:01 +0200

    Add header and footer

--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
-import _ from "lodash";
 import "./style.css";
-function component() {
-  const element = document.createElement("div");
-
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-
-  return element;
-}
-
-document.body.appendChild(component());
+import "bootstrap/dist/css/bootstrap.min.css";
