00a7293ea975c92828c49afc068badf83bc23705
amira <amiraabouhadid@gmail.com>
2022-03-07 17:28:48 +0200

    remove linter errors using eslint --fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import "./style.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import populateHome from "./modules/Home";
+import './style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import populateHome from './modules/Home';
 
 populateHome();
