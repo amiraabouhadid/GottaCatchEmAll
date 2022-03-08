d6b10eecd91f49f92bb4c68671fc68b00e604373
Amar Hamlaoui <amarhamlaoui@gmail.com>
2022-03-08 19:25:20 +0100

    Count comments and display them for each item.

--- src/utils/commentsHandler.js
+++ src/utils/commentsHandler.js
@@ -1,28 +1,42 @@
-const appId = 'OJhoS4niRmFdRpqldNlB';
+import commentsCounter from './commentsCounter';
+
+const appId = "OJhoS4niRmFdRpqldNlB";
 const commentsURL = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/comments`;
 
 const addComment = async (comment) => {
   await fetch(commentsURL, {
-    method: 'POST',
+    method: "POST",
     body: JSON.stringify(comment),
     headers: {
-      'Content-type': 'application/json; charset=UTF-8',
+      "Content-type": "application/json; charset=UTF-8",
     },
   });
 };
 
 const getComments = async (i) => {
   const commentsContainer = document.getElementById(`comments${i}`);
-  commentsContainer.innerHTML = '';
-  await fetch(`${commentsURL}?item_id=item${i}`)
-    .then((response) => response.json())
-    .then((comments) => {
-      comments.forEach((comment) => {
-        const commentText = document.createElement('p');
+  commentsContainer.innerHTML = "";
+  let myPromise = new Promise((resolve, reject) => {
+      fetch(`${commentsURL}?item_id=item${i}`)
+        .then((response) => {
+          console.log(response);
+          if (response.ok){
+            resolve(response.json());
+          }
+        })
+        .catch( () => {console.log(err)})
+    });
+    myPromise.then(
+      function(value) { 
+        value.forEach((comment) => {
+        const commentText = document.createElement("p");
         commentText.innerHTML = `${comment.creation_date} ${comment.username}: ${comment.comment}`;
         commentsContainer.appendChild(commentText);
-      });
-    });
+        }); 
+        commentsCounter(i);
+      }
+    );
+
 };
 
-export {addComment, getComments}
+export { addComment, getComments };
