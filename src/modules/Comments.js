fa04d9c38b3ed1730476fc834389545ce34621fa
Amar Hamlaoui <amarhamlaoui@gmail.com>
2022-03-08 13:22:30 +0100

    Fix linters errors.

--- src/modules/Comments.js
+++ src/modules/Comments.js
@@ -5,14 +5,14 @@ export const getComments = async (i) => {
   const commentsContainer = document.getElementById(`comments${i}`);
   commentsContainer.innerHTML = '';
   await fetch(`${commentsURL}?item_id=item${i}`)
-          .then(response => response.json())
-          .then((comments) => {
-            comments.forEach((comment) => {
-              const commentText = document.createElement('p');
-              commentText.innerHTML = `${comment.creation_date} ${comment.username}: ${comment.comment}`;
-              commentsContainer.appendChild(commentText);
-            })
-          });
+    .then((response) => response.json())
+    .then((comments) => {
+      comments.forEach((comment) => {
+        const commentText = document.createElement('p');
+        commentText.innerHTML = `${comment.creation_date} ${comment.username}: ${comment.comment}`;
+        commentsContainer.appendChild(commentText);
+      });
+    });
 };
 
 const comments = (pokemon, commentLink, i) => {
@@ -83,7 +83,7 @@ const comments = (pokemon, commentLink, i) => {
                             </div>`;
   container.appendChild(commentPopup);
   commentPopup.addEventListener('show.bs.modal', () => {
-    getComments(i,container);
+    getComments(i, container);
   });
 };
 
