5cb1e023a906943b2b74e3dc8b2019a463da3fa2
amira <amiraabouhadid@gmail.com>
2022-03-08 18:54:02 +0200

    add textdecoration to navlinks based on current page

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import Nav from './modules/Nav';
 const baseURL = 'https://pokeapi.co/api/v2/';
-const homeURL = `${baseURL}pokemon?offset=20&limit=20`;
+const homeURL = `${baseURL}pokemon?offset=20&limit=48`;
 const typeURL = `${baseURL}type`;
-const abilityURL = `${baseURL}ability`;
+const abilityURL = `${baseURL}ability?offset=20&limit=48`;
 Nav(baseURL, homeURL, typeURL, abilityURL);
--- src/utils/populatePage.js
+++ src/utils/populatePage.js
@@ -7,6 +7,10 @@ const populatePage = async (requestURL) => {
   const appId = 'FK4BafgXZ3oaEOpMgby6';
   const likesURL = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/likes`;
   let likesArr;
+  let itemsCount;
+  const homeLink = document.getElementById('home');
+  const typesLink = document.getElementById('types');
+  const abilitiesLink = document.getElementById('abilities');
   await fetch(likesURL)
     .then((response) => response.json())
     .then((json) => {
@@ -14,17 +18,44 @@ const populatePage = async (requestURL) => {
     });
   await fetch(requestURL)
     .then((response) => response.json())
-    .then((json) => json.results.forEach((entity, i) => {
-      const item = new Item(entity.name, entity.url, `${entity.name + i}`, 0);
+    .then((json) => {
+      itemsCount = json.results.length;
+      if(requestURL.includes('pokemon')){
+        homeLink.innerHTML = `Pokemons (${itemsCount})`;
+        homeLink.style.textDecoration ='underline';
+        abilitiesLink.style.textDecoration ='none';
+        typesLink.style.textDecoration ='none';
+        typesLink.innerHTML = `Types`;
+        abilitiesLink.innerHTML = `Abilities`;
+      }else if (requestURL.includes('type')){
+        homeLink.innerHTML = `Pokemons`;
+        typesLink.innerHTML = `Types (${itemsCount})`;
+        typesLink.style.textDecoration ='underline';
+        homeLink.style.textDecoration ='none';
+        abilitiesLink.style.textDecoration ='none';
+        abilitiesLink.innerHTML = `Abilities`;
+      }else{
+        homeLink.innerHTML = `Pokemons`;
+        typesLink.innerHTML = `Types`;
+        abilitiesLink.innerHTML = `Abilities (${itemsCount})`;
+        abilitiesLink.style.textDecoration ='underline';
+        typesLink.style.textDecoration ='none';
+        homeLink.style.textDecoration ='none';
 
-      const itemsLikes = likesArr.filter(
-        (el) => el.item_id === `${entity.name + i}`,
-      );
-      if (itemsLikes.length > 0) {
-        item.likes = itemsLikes[0].likes;
       }
-
-      createCard(item, container, i, requestURL);
-    }));
+      console.log(itemsCount);
+      json.results.forEach((entity, i) => {
+        const item = new Item(entity.name, entity.url, `${entity.name + i}`, 0);
+        const itemsLikes = likesArr.filter(
+          (el) => el.item_id === `${entity.name + i}`,
+        );
+        if (itemsLikes.length > 0) {
+          item.likes = itemsLikes[0].likes;
+        }
+  
+        createCard(item, container, i, requestURL);
+        
+      })
+    });
 };
 export default populatePage;
